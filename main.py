e7e6282d7562a2d5b9de3042e59fbcb1038c84ad
Tejas Dnyaneshwar Balshetwar <tejas.balshetwar@hexaviewtech.com>
2023-02-27 16:42:28 +0530

    code basic document CRUD

--- main.py
+++ main.py
@@ -37,4 +37,51 @@ es = Elasticsearch(
 # except Exception as e:
 #     print("Index does not exist")
 
+# inserting documents
+# employees_data = [
+#     {
+#     'name': 'Vishnu',
+#     'age': 21,
+#     'programming_languages': ['C++', 'python', 'nodejs']
+#     },
+#     {
+#     'name': 'Sanjay',
+#     'age': 23,
+#     'programming_languages': ['python', 'C#']
+#     },
+#     {
+#     'name': 'Arjun',
+#     'age': 33,
+#     'programming_languages': ['C++', 'Ruby']
+#     },
+#     {
+#     'name': 'Ram',
+#     'age': 27,
+#     'programming_languages': ['Rust', 'python']
+#     }
+# ]
+# for data in employees_data: es.index(index='employees', document=data)
 
+
+# get a document
+# res = es.get(index="employees", id='O-p-koYBJdUnV4UuZxFA') # fetch doc
+# print(res['_source'])
+
+
+# update a document
+# es.update(index="employees", id='O-p-koYBJdUnV4UuZxFA', doc={'country': 'US'},)
+# res = es.get(index="employees", id='O-p-koYBJdUnV4UuZxFA')
+# print(res)
+
+# delete a document
+# res = es.delete(index="employees", id='O-p-koYBJdUnV4UuZxFA')
+# print(res)
+
+# basic term query
+# print(es.search(index="employees", query={"match": {'name':'Sanjay'}}))
+
+# full-text search
+# print(es.search(index="employees", query={"fuzzy": {'programming_languages':'python'}}))
+
+# regex search
+print(es.search(index="employees", query={"regexp": {'name':'.*n.*'}}))
