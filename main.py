81e5c5e6823588c175b297cec4e120ce26992a32
Tejas Dnyaneshwar Balshetwar <tejas.balshetwar@hexaviewtech.com>
2023-02-28 17:03:10 +0530

    search section complete

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from elasticsearch import Elasticsearch
 
 # connection to elastic
 es = Elasticsearch(
-            ["https://elastic:c1+=-8fC3kMvGJ-JLb_J@localhost:9200"],
-            use_ssl=True,
-            verify_certs=True,
-            ca_certs='./first/config/certs/http_ca.crt',
-        )
+    ["https://elastic:c1+=-8fC3kMvGJ-JLb_J@localhost:9200"],
+    use_ssl=True,
+    verify_certs=True,
+    ca_certs='./first/config/certs/http_ca.crt',
+)
 
 # verify connection
 # print(es.ping())
@@ -117,15 +117,86 @@ es = Elasticsearch(
 # searching multiple fields
 # print(es.search(index="employees",query={"multi_match":{"query":"ruby","fields":["name","programming_languages"]}})) # searches for word in both name and programming_languages field
 # We can also boost relevance score for a particular field which will give more importance to that field
-# a tie_breaker attribute can also be used which will basically multiply the relevance score of the lower score field with what we give the arguement and will add to the field with higher relevance score 
+# a tie_breaker attribute can also be used which will basically multiply the relevance score of the lower score field with what we give the arguement and will add to the field with higher relevance score
 
 # phrase searches
 # print(es.search(index="employees",query={"match_phrase":{"name":"arjun nepal"}}))
 # in match_phrase the order of the words entered matters and in consecutive manner unlike the match query
 
+# boolean compound query using must and must_not
+# print(es.search(index="employees", query={"bool": {"must": {"term":{"programming_languages.keyword":"Python"}} }})) # must the query parameter
+
+# print(es.search(index="employees", query={"bool": {"must": {"term":{"programming_languages.keyword":"python"}},"must_not": {"term":{"programming_languages.keyword":"nodejs"}} }})) # musn't contain the query paramter
+
+# boolean compound query using should which will boost the score if found true
+# print(es.search(index="employees", query={"bool": {"should": [{"term":{"programming_languages.keyword":"python"}}],"must": [{"term":{"programming_languages.keyword":"python"}}],"must_not": [{"term":{"programming_languages.keyword":"nodejs"}}] } }))
+
+
+# boolean compound query using filter - similar to must difference is that filter ignores relevance scores
+# filter is highly efficient and can be used to cache results
+# print(es.search(index="employees",query={"bool":{"filter":{"term":{"programming_languages.keyword":"python"}}}}))
+
+
+# SELECT * FROM employees WHERE programming_languages IN ("python") AND (name LIKE '%python%' OR description LIKE '%python%') AND age <= 30
+# print(es.search(index="employees",query={
+#     "bool": {
+#         "filter": [
+#             {
+#                 "range": {
+#                     "age": {
+#                         "lte": 30
+#                     }
+#                 }
+#             },
+#             {
+#                 "term": {
+#                     "programming_languages.keyword": "python"
+#                 }
+#             }
+#         ],
+#         "must": [
+#             {
+#                 "multi_match": {
+#                     "query": "python",
+#                     "fields": [
+#                         "name",
+#                         "programming_languages"
+#                     ]
+#                 }
+#             }
+#         ]
+#     }
+# }))
+
+# boosting query- changing 
+# print(es.search(index="employees",query={
+#     "boosting": {
+#       "positive": {
+#         "match_all": {}
+#       },
+#       "negative": {
+#         "match": {
+#           "name": "arjun"
+#         }
+#       },
+#       "negative_boost": 0.5
+#     }
+#   }))
+
+
+# disjunction max
+# print(es.search(index="employees",query={
+#     "dis_max": {
+#       "queries": [
+#         { "match": { "name": "python" } },
+#         { "match": { "programming_language": "python" } }
+#       ],
+#       "tie_breaker": 0.3
+#     }
+# }))
+
+
 
 
 # fuzzy search
 # print(es.search(index="employees", query={"fuzzy": {'programming_languages':'python'}}))
-
-
